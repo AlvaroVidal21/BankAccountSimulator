f8ab8f2bc4646bdc860d5e8a08b9424afae97173
AlvaroVidal <alvaro.vidal1@unmsm.edu.pe>
2024-03-23 10:48:39 -0500

    Creado la tabla de users

--- database_scripts/insert_users.py
+++ database_scripts/insert_users.py
@@ -22,7 +22,9 @@ def create_user_table(db):
         CREATE TABLE IF NOT EXISTS users(
             id_user INTEGER PRIMARY KEY AUTOINCREMENT,
             name_user TEXT NOT NULL,
+            age_user INTEGER NOT NULL,
             district_name INTEGER NOT NULL,
+            status_district TEXT NOT NULL,
             balance_user REAL NOT NULL,
             debt_user REAL NOT NULL,
             status_debt TEXT NOT NULL
@@ -38,27 +40,36 @@ def insert_users(db, csv):
     conn = sqlite3.connect(db)
     cursor = conn.cursor()
 
-    query = """
-        INSERT INTO users (name_user, district_name, balance_user, debt_user, status_debt) VALUES (?, ?, ?, ?, ?)
-    """
+    cursor.execute("SELECT COUNT(*) FROM users")
+    rows = cursor.fetchone()[0]
 
-    with open(csv, "r") as file:
-        rows = 0
-        for line in file:
-            rows += 1
-            if rows == 1:
-                continue
-            else:
-                data = line.strip().split(",")
-                name_user = data[0]
-                district_name = data[1]
-                balance_user = data[2]
-                debt_user = data[3]
-                status_debt = data[4]
+    if rows == 0:
+        query = """
+            INSERT INTO users (name_user, age_user, district_name, status_district, balance_user, debt_user, status_debt) VALUES (?, ?, ?, ?, ?, ?, ?)
+        """
 
-                district_name_id = dictionary_districts.get(district_name)
+        with open(csv, "r") as file:
+            rows = 0
+            for line in file:
+                rows += 1
+                if rows == 1:
+                    continue
+                else:
+                    data = line.strip().split(",")
+                    name_user = data[1]
+                    name_age = data[2]
+                    district_name = data[3].strip()
+                    status_district = data[4]
+                    balance_user = data[5]
+                    debt_user = data[6]
+                    status_debt = data[7]
 
-                cursor.execute(query, (name_user, district_name_id, balance_user, debt_user, status_debt))
+                    district_name_id = dictionary_districts.get(district_name)
+
+                    cursor.execute(query, (name_user, name_age, district_name_id, status_district, balance_user, debt_user, status_debt))
+            conn.commit()
+    conn.close()
+
 
                     
 
--- test.py
+++ test.py
@@ -1,22 +1,40 @@
 import random
 
-dictionary_districts = {
-    "San Isidro": 1,
-    "Miraflores": 2,
-    "Jesus Maria": 3,
-    "Lince": 4,
-    "Chosica": 5,
-    "Chaclacayo": 6,
-    "San Juan de Lurigancho": 7,
-    "Cieneguilla": 8,
-}
+
 
 def doing():
-    r = dictionary_districts["San Isidro"]
-    print(r)
-    print("\n")
-    a = dictionary_districts.get("San Isidro")
-    print(a)
+    dictionary_districts = {
+        "San Isidro": 1,
+        "Miraflores": 2,
+        "Jesus Maria": 3,
+        "Lince": 4,
+        "Chosica": 5,
+        "Chaclacayo": 6,
+        "San Juan de Lurigancho": 7,
+        "Cieneguilla": 8,
+    }
+    with open("accounts.csv", "r") as file:
+        lista = []
+        rows = 0
+        for line in file:
+            rows += 1
+            if rows == 1:
+                continue
+            else:
+                data = line.strip().split(",")
+                name_user = data[1]
+                name_age = data[2]
+                district_name = data[3].strip()
+                status_district = data[4]
+                balance_user = data[5]
+                debt_user = data[6]
+
+                district_name_id = dictionary_districts.get(district_name)
+
+                lista.append(district_name_id)
+                lista.append(district_name)
+
+        print(lista)
    
                 
 
